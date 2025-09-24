3bbb0ce30212191c6f35c2ef86367ba50627f1b8
Dikshant Rajput <dikshant4848@gmail.com>
2025-09-24 09:45:57 +0530

    Remove node_modules from repo

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET || 'secret123';
-
+const bcrypt = require('bcryptjs');
 function authenticate(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ message: 'Authorization header missing' });
